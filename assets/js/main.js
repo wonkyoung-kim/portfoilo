1dca5082c3abc895106af6d7b55e7f81c3d4b250
wonkyoun-kim <wwoj0205@gmail.com>
2022-11-12 16:25:59 +0900

    마크업 진행중

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(window).on('load', function(){
+  mainScript.swiperFnc();
+  mainScript.clickFnc();
+  mainScript.scrollFnc();
+  mainScript.mouseFnc();
+  mainScript.gsapFnc();
+})
+
+const mainScript = {
+  
+}
